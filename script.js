b1aae88a838beb91d107262074289c2d427a8c07
RitAI-zbs <herrcyber@gmail.com>
2026-07-20 09:32:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,20 @@ document.querySelectorAll(".hidden").forEach(el=>{
 observer.observe(el);
 
 });
+
+//================ HERO PARALLAX ================
+
+const hero = document.querySelector(".hero");
+
+document.addEventListener("mousemove",(e)=>{
+
+const x=(window.innerWidth/2-e.clientX)/40;
+
+const y=(window.innerHeight/2-e.clientY)/40;
+
+hero.style.backgroundPosition=
+
+`calc(50% + ${x}px)
+ calc(50% + ${y}px)`;
+
+});
