01c2d3175d5703e6cd856cac17b97d269912915a
RitAI-zbs <herrcyber@gmail.com>
2026-07-20 08:42:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ function changeImage(img){
 document.getElementById("mainImage").src=img.src;
 
 }
+
+// ---------- HEADER ----------
+
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 80)
+        header.classList.add("scrolled");
+    else
+        header.classList.remove("scrolled");
+
+});
